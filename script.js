d7d7e30b0b1abb39998b951ff41e1b28ac5ef161
KVKYES <winstontroysings@gmail.com>
2016-02-21 21:55:33 -0500

    Pseudocode attempt

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+var city = ?;
+
+$('.entry').on('submit', function(event){
+  e.preventDefault();
+  addBackgroundImage();
+})
+
+
+if (user submits "Austin" or "ATX"){
+ $('body').css('backgroundImage',../images/austin.jpg);
+}
+
+else if (user submits "Los Angeles" or "LA" or "LAX"){
+ $('body').css('backgroundImage',../images/la.jpg);
+}
+
+else if (user submits "New York" or "New York City" or "NYC"){
+ $('body').css('backgroundImage',../images/nyc.jpg);
+}
+
+else if (user submits "San Francisco" or "SF" or "Bay Area"){
+ $('body').css('backgroundImage',../images/sf.jpg);
+}
+
+else if (user submits "Sydney" or "SYD"){
+ $('body').css('backgroundImage',../images/sydney.jpg);
+}
+
+});
